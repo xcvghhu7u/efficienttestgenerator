3af48e2ac1b965eec0bbcfba6966b601683a1d74
xcvghhu7u <gs1bahabah1@hotmail.com>
2024-05-07 23:48:15 +0700

    aptos commit

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 function maximalSquare(matrix) {
-  if (matrix.length === 0) return 0;
+  if (matrix.length === 0 || matrix[0].length === 0) return 0;
   const rows = matrix.length;
   const cols = matrix[0].length;
-  const dp = Array.from(Array(rows + 1), () => Array(cols + 1).fill(0));
-  let maxSide = 0;
+  const dp = Array.from({ length: rows + 1 }, () =>
+    Array.from({ length: cols + 1 }, () => 0),
+  );
+  let maxSquare = 0;
   for (let i = 1; i <= rows; i++) {
     for (let j = 1; j <= cols; j++) {
       if (matrix[i - 1][j - 1] === "1") {
-        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
-        maxSide = Math.max(maxSide, dp[i][j]);
+        dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1;
+        maxSquare = Math.max(maxSquare, dp[i][j]);
       }
     }
   }
-  return maxSide * maxSide;
+  return maxSquare * maxSquare;
 }
