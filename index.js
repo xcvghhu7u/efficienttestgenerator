5b7db6a313a4fcf00a8f23fe2e36b1e10038ea8f
xcvghhu7u <gs1bahabah1@hotmail.com>
2024-05-09 21:58:58 +0700

    update sdk task

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-function lengthOfLIS(nums) {
-  if (nums.length === 0) return 0;
-  const dp = new Array(nums.length).fill(1);
-  let max = 1;
-  for (let i = 1; i < nums.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (nums[i] > nums[j]) {
-        dp[i] = Math.max(dp[i], dp[j] + 1);
-        max = Math.max(max, dp[i]);
-      }
-    }
+function canJump(nums) {
+  let lastPos = nums.length - 1;
+  for (let i = nums.length - 2; i >= 0; i--) {
+    if (i + nums[i] >= lastPos) lastPos = i;
   }
-  return max;
+  return lastPos === 0;
 }
