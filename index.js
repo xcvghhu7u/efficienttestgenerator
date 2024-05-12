661bbc6e597f4b0cd6a9955a55f110ec41dc3cc7
xcvghhu7u <gs1bahabah1@hotmail.com>
2024-05-12 10:15:42 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-function numDecodings(s) {
-  const dp = new Array(s.length + 1).fill(0);
-  dp[0] = 1;
-  dp[1] = s[0] === "0" ? 0 : 1;
-  for (let i = 2; i <= s.length; i++) {
-    const oneDigit = parseInt(s.substring(i - 1, i));
-    const twoDigits = parseInt(s.substring(i - 2, i));
-    if (oneDigit >= 1) {
-      dp[i] += dp[i - 1];
-    }
-    if (twoDigits >= 10 && twoDigits <= 26) {
-      dp[i] += dp[i - 2];
-    }
+const factorial = (n) => {
+  if (n === 0 || n === 1) {
+    return 1;
   }
-  return dp[s.length];
-}
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+};
